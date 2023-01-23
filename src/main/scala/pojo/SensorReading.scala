49b835fbd63dad4ede96ed819716bd59c24f8ba4
alipay <alipay@aliyun.com>
2023-01-23 22:37:10 +0800

    update

--- src/main/scala/pojo/SensorReading.scala
+++ src/main/scala/pojo/SensorReading.scala
@@ -3,7 +3,7 @@ package pojo
 /**
  * @Author Master
  * @Date 2021/10/17
- * @Time 14:10
+ * @Time 14:11
  * @Name flink
  */
 case class SensorReading(id: String, timestamp: Long, temperature: Double)
